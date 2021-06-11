32e83276ef7e5160172bc3644c4c878b32deb059
monget <tristan.monget@laposte.net>
2021-06-11 18:07:42 +0200

    addition of checkPublication middelware on the roads

--- backend/routes/publications.js
+++ backend/routes/publications.js
@@ -3,14 +3,15 @@ const router = express.Router();
 
 const publicationCrtl = require('../controllers/publications');
 const multer = require('../middleware/multer-config');
+const checkPublication = require('../middleware/checkPublication');
 const validator = require('../middleware/validator');
 const auth = require('../middleware/auth');
 
-router.post('/',/* auth,*/ multer,/* (req, res, next) => {req.body = JSON.parse(req.body.sauce), next()}, validator.createSauce,*/ publicationCrtl.create);
-router.put('/:id',/* auth, multer, (req, res, next) => {if (req.file != undefined) {req.body = JSON.parse(req.body.sauce)} next()}, validator.modifySauce,*/ publicationCrtl.modify);
-router.delete('/:id',/* auth,*/ publicationCrtl.delete);
-router.get('/:id',/* auth,*/ publicationCrtl.findOne);
+router.post('/',/* auth,*/ multer,/* (req, res, next) => {req.body = JSON.parse(req.body.publication), next()}, validator.create,*/ publicationCrtl.create);
+router.put('/:id',/* auth,*/ checkPublication,/* multer, (req, res, next) => {if (req.file != undefined) {req.body = JSON.parse(req.body.publication)} next()}, validator.modify,*/ publicationCrtl.modify);
+router.delete('/:id',/* auth,*/ checkPublication, publicationCrtl.delete);
+router.get('/:id',/* auth,*/ checkPublication, publicationCrtl.findOne);
 router.get('/',/* auth,*/ publicationCrtl.findAll);
-router.post('/:id/like',/* auth,*/ publicationCrtl.like);
+router.post('/:id/like',/* auth,*/ checkPublication, publicationCrtl.like);
 
 module.exports = router;
