c3fb20be706e55ab7d6205ab8559525002b0eaea
monget <tristan.monget@laposte.net>
2021-06-17 16:33:54 +0200

    modification of User.findOne

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,23 +1,24 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const db = require("../models");
+const User = db.user;
 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        const userId = decodedToken.userId;
-        User.findOne({ id: userId })
-            .then(user => {
-                if (!user) {
-                    throw 'User ID non valable';
-                }
-                else {
-                    next();
-                }
-            })
-            .catch(error => res.status(401).json({ error }))
+  try {
+    const token = req.headers.authorization;
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decodedToken.userId;
+    User.findOne({ where: { id: userId }})
+      .then(user => {
+        if (!user) {
+          throw 'User ID non valable';
+        }
+        else {
+          next();
+        }
+      })
+      .catch(error => res.status(401).json({ error }))
     }
-    catch (error) {
-        res.status(401).json({ error: 'Requête non authentifié !' })
-    }
+  catch (error) {
+    res.status(401).json({ error: 'Requête non authentifié !' })
+  }
 }
