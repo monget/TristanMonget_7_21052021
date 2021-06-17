813340d4f7b15c8fe1c97cef9a766d0dd81bff40
monget <tristan.monget@laposte.net>
2021-06-17 16:37:30 +0200

    Added comment and foreignKey

--- backend/models/index.js
+++ backend/models/index.js
@@ -26,5 +26,33 @@ db.sequelize = sequelize;
 
 db.user = require("../models/user.js")(sequelize, Sequelize);
 db.publication = require("../models/publication.js")(sequelize, Sequelize);
+db.comment = require("../models/comment.js")(sequelize, Sequelize);
+
+db.user.hasMany(db.publication, {
+  foreignKey: {
+    allowNull: false
+  },
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+});
+db.publication.belongsTo(db.user);
+
+db.publication.hasMany(db.comment, {
+  foreignKey: {
+    allowNull: false
+  },
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+});
+db.comment.belongsTo(db.publication);
+
+db.user.hasMany(db.comment, {
+  foreignKey: {
+    allowNull: false
+  },
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+});
+db.comment.belongsTo(db.user);
 
 module.exports = db;
