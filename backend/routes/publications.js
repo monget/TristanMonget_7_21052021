8fcab2e13a7c287ef0ec1afb48e4e9fd18d9dc8d
monget <tristan.monget@laposte.net>
2021-06-19 19:13:06 +0200

    updated routes

--- backend/routes/publications.js
+++ backend/routes/publications.js
@@ -7,8 +7,8 @@ const check = require('../middleware/checkExist');
 const validator = require('../middleware/validator');
 const auth = require('../middleware/auth');
 
-router.post('/', auth, multer,/* (req, res, next) => {req.body = JSON.parse(req.body.publication), next()}, validator.create,*/ publicationCrtl.create);
-router.put('/:id', auth, check.publication, multer,/* (req, res, next) => {if (req.file != undefined) {req.body = JSON.parse(req.body.publication)} next()}, validator.modify,*/ publicationCrtl.modify);
+router.post('/', auth, multer, validator.createPublication, publicationCrtl.create);
+router.put('/:id', auth, check.publication, multer, validator.modifyPublication, publicationCrtl.modify);
 router.delete('/:id', auth, check.publication, publicationCrtl.delete);
 router.get('/:id', auth, check.publication, publicationCrtl.findOne);
 router.get('/', auth, publicationCrtl.findAll);
