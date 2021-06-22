7905d9bcecf8f655b628634f71f4b4d41947c6f4
monget <tristan.monget@laposte.net>
2021-06-22 23:16:13 +0200

    Updated routes

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -7,11 +7,10 @@ const check = require('../middleware/checkExist');
 const validator = require('../middleware/validator');
 const auth = require('../middleware/auth');
 
-router.post('/', auth, multer,/* (req, res, next) => {req.body = JSON.parse(req.body.publication), next()}, validator.create,*/ commentCrtl.create);
-router.put('/:id', auth, check.comment, multer,/* (req, res, next) => {if (req.file != undefined) {req.body = JSON.parse(req.body.publication)} next()}, validator.modify,*/ commentCrtl.modify);
+router.post('/', auth, multer, validator.createComment, commentCrtl.create);
+router.put('/:id', auth, check.comment, multer, validator.modifyComment, commentCrtl.modify);
 router.delete('/:id', auth, check.comment, commentCrtl.delete);
 router.get('/:id', auth, check.comment, commentCrtl.findOne);
-router.get('/', auth, commentCrtl.findAll);
 router.post('/:id/like', auth, check.comment, commentCrtl.like);
 
 module.exports = router;
