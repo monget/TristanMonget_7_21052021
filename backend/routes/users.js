ed7d38885489518aef26ff7fb62b80f3e9cb1916
monget <tristan.monget@laposte.net>
2021-06-24 22:37:45 +0200

    added route put for profil

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,8 +5,10 @@ const userCtrl = require('../controllers/users');
 const validator = require('../middleware/validator');
 const check = require('../middleware/checkExist');
 const accountLimit = require('../middleware/accountLimiter');
+const auth = require('../middleware/auth');
 
 router.post('/signup', accountLimit, check.user, validator.signup, userCtrl.signup);
 router.post('/login', accountLimit, userCtrl.login);
+router.put('/profil/:id', auth, validator.signup, check.user, userCtrl.editProfil);
 
 module.exports = router;
