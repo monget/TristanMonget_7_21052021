687084439adf5fc9049546477d81cd757ae51605
monget <tristan.monget@laposte.net>
2021-06-24 22:35:21 +0200

    added controller for edit profil

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 const User = db.user;
+const userId = require("../utils/userId.js");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
@@ -40,4 +41,23 @@ exports.login = (req, res, next) => {
       })
     })
     .catch(err => res.status(500).send({ message: err.message }));
+};
+
+exports.editProfil = (req, res, next) => {
+  User.findByPk(req.params.id)
+    .then(user => {
+      if (user.id == userId(req)) {
+        User.update({
+          pseudo: req.body.pseudo,
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password, 10)
+        },
+        { where: { id: req.params.id }})
+          .then(() => res.status(200).json({ message: 'Profil modifié !'}))
+          .catch(err => res.status(500).send({ message: err.message }));
+      }
+      else {
+        return res.status(400).json({ message: "Opération interdite !"})
+      }
+    })
 };
