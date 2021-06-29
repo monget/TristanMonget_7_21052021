19953d652c7f4bc05cf1801e4b2447ddfc602b03
monget <tristan.monget@laposte.net>
2021-06-29 22:28:09 +0200

    added like/dislike for publications

--- backend/controllers/publications.js
+++ backend/controllers/publications.js
@@ -4,9 +4,8 @@ const userId = require("../utils/userId.js");
 const Publication = db.publication;
 const Comment = db.comment;
 const User = db.user;
+const Like = db.like;
 const fs = require('fs');
-const publication = require("../models/publication");
-const { createPublication } = require("../middleware/validator");
 
 exports.create = (req, res, next) => {
   Publication.create({
@@ -14,6 +13,7 @@ exports.create = (req, res, next) => {
 		message: req.body.message,
     attachement: `${req.protocol}://${req.get('host')}/images/publications/${req.file.filename}`,
 		like: 0,
+		dislike: 0,
 		userId: userId(req)
   })
     .then(() => res.status(201).json({ message: 'Publication créé !' }))
@@ -109,8 +109,6 @@ exports.findAll = (req, res, next) => {
 		.catch(err =>	res.status(400).send({ message: err.message }));
 };*/
 
-
-
 exports.findAll = (req, res, next) => {
 	Publication.findAll({
 		attributes: {
@@ -167,21 +165,95 @@ exports.findAll = (req, res, next) => {
 		.catch(err =>	res.status(400).send({ message: err.message }));
 };
 
-
 exports.like = (req, res, next) => {
 	counter = req.body.like;
-	Publication.findByPk(req.params.id)
+	Publication.findByPk(req.params.id, {	include: [{ model: Like, attributes: ['usersLiked', 'usersDisliked', 'userId', 'publicationId'], required: false }] })
 		.then(publication => {
-			if (counter === 1) {
-				publication.increment({ like: 1 }, { where: { id: req.params.id }})
-					.then(() => res.status(200).json({ message: 'J\'aime !' }))
-					.catch(err => res.status(400).send({ message: err.message }));
-			}
-			else if (counter === 0) {
-				publication.increment({ like: -1 }, {	where: { id: req.params.id }})
-					.then(() => res.status(200).json({ message: 'J\'aime annulé' }))
-					.catch(err => res.status(400).send({ message: err.message }));
-			}
+			Like.findAll({ where: { publicationId: req.params.id, userId: userId(req)}})
+				.then(likes => {
+					const arrayUserId = [];
+					publication.likes.forEach(likes => {
+						arrayUserId.push(likes.dataValues.userId)
+					})
+					if (arrayUserId.includes(userId(req))) {
+						likes.forEach(like => {
+							if (counter === 1) {
+								if (like.dataValues.usersDisliked === true) {
+									Like.update({ usersLiked: true, usersDisliked: false }, { where: { userId: userId(req), publicationId: req.params.id }})
+										.then(() => {
+											publication.increment({ like: 1, dislike: 1 }, {	where: { id: req.params.id }})
+												.then(() => res.status(200).json({ message: 'Finalement j\'aime ! !' }))
+												.catch(err => res.status(400).send({ message: err.message }));
+									})
+								}
+								else {
+									res.status(400).json({ message: 'j\'aime déjà cette publication !' })
+								}
+							}
+							else if (counter === 0) {
+								if (like.dataValues.usersLiked === true) {
+									Like.destroy({ where: { userId: userId(req), publicationId: req.params.id }})
+										.then(() => {
+											publication.increment({ like: -1 }, { where: { id: req.params.id }})
+												.then(() => res.status(200).json({ message: 'J\'aime annulé'}))
+												.catch(error => res.status(400).json({ error }));
+										})
+								}
+								else {
+									Like.destroy({ where: { userId: userId(req), publicationId: req.params.id }})
+										.then(() => {
+											publication.increment({ dislike: 1 }, { where: { id: req.params.id }})
+												.then(() => res.status(200).json({ message: 'Je n\'aime pas annulé'}))
+												.catch(error => res.status(400).json({ error }));
+										})
+								}
+							}
+							else if (counter === -1) {
+								if (like.dataValues.usersLiked === true) {
+									Like.update({ usersLiked: false, usersDisliked: true }, { where: { userId: userId(req), publicationId: req.params.id }})
+										.then(() => {
+											publication.increment({ like: -1, dislike: -1 }, {	where: { id: req.params.id }})
+												.then(() => res.status(200).json({ message: 'Je n\'aime plus !' }))
+												.catch(err => res.status(400).send({ message: err.message }));
+										})
+								}
+								else {
+									res.status(400).json({ message: 'Je n\'aime pas déjà cette publication !' })
+								}
+							}
+						});
+					}
+					else {
+						if (counter === 1) {
+							publication.increment({ like: 1 }, { where: { id: req.params.id }})
+								.then(() => {
+									Like.create({
+										usersLiked: true,
+										userId: userId(req),
+										publicationId: req.params.id
+									})
+									res.status(200).json({ message: 'J\'aime !' })
+								})
+								.catch(err => res.status(400).send({ message: err.message }));
+						}
+						else if (counter === -1) {
+							publication.increment({ dislike: -1 }, {	where: { id: req.params.id }})
+							.then(() => {
+								Like.create({
+									usersDisliked: true,
+									userId: userId(req),
+									publicationId: req.params.id
+								})
+								res.status(200).json({ message: 'Je n\'aime pas !' })
+							})
+							.catch(err => res.status(400).send({ message: err.message }));
+						}
+						else if (counter === 0) {
+							res.status(400).json({ message: 'Opération impossible' })
+						}
+					}
+				})
+				.catch(err => res.status(400).send({ message: err.message }));
 		})
 		.catch(err => res.status(404).send({ message: err.message }));
 };
