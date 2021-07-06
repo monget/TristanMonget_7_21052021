de9a269de309d31ca145279d87a5198bb0856a14
monget <tristan.monget@laposte.net>
2021-07-06 15:44:26 +0200

    import vee-validate in global

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { ValidationProvider, ValidationObserver } from 'vee-validate';
+import './services/ValidationForm'
+
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
+
 Vue.config.productionTip = false
 
 new Vue({
