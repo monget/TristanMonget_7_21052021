d9cac25869ecc0c690e7688a95e95798287dc0ca
monget <tristan.monget@laposte.net>
2021-07-08 17:22:35 +0200

    add split for bearer token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ const User = db.user;
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decodedToken.userId;
     User.findOne({ where: { id: userId }})
