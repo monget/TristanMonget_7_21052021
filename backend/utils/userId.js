f270d0a940a47c04380a7111b11006b2ea3cfee3
monget <tristan.monget@laposte.net>
2021-07-08 17:22:47 +0200

    add split for bearer token

--- backend/utils/userId.js
+++ backend/utils/userId.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-	const token = req.headers.authorization;
+	const token = req.headers.authorization.split(' ')[1];
 	const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 	const userId = decodedToken.userId;
 	
