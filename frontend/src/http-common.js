486b6f2edf8e57655721dc4ce7b3f4994be98dc5
monget <tristan.monget@laposte.net>
2021-07-08 17:29:24 +0200

    auth headers integration in axios

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 
+function authHeader() {
+  if (localStorage.user) {
+    let user = JSON.parse(localStorage.getItem('user'));
+    
+    if (user.token) {
+      return { Authorization: 'Bearer ' + user.token };
+    }
+    else {
+      return {};
+    }
+  }
+}
+
 export default axios.create({
   baseURL: "http://localhost:3000/api",
-  headers: {
-    "Content-type": "application/json"
-  }
+  headers: authHeader(), "Content-type": "application/json"
 });
