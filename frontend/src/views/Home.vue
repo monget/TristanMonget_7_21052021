03f6c1c243967468f3945a7a7fc8a16de3a8e364
monget <tristan.monget@laposte.net>
2021-07-12 18:09:29 +0200

    in course

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,42 +1,68 @@
 <template>
-  <div>
-    <button>Publiez quelque chose...</button>
+  <main class="home">
+    <div class="publish">
+      <button>Publiez quelque chose...</button>
+    </div>
     <div class="publication">
-      <div class="publication__wrap" v-for="publication in publications" :key="publication.id">
-        <a href="#">
+      <div class="publication__wrap" v-for="(publication, index) in publications" :key="index">
         <div class="publication__head">
           <div>
-            {{ publication.publishedBy }} le {{ publication.createdAt }} à {{  }}
+            {{ publication.publishedBy }} le {{ formatDate(publication.createdAt) }}
           </div>
-          <button>...</button>
+          <button v-if="access(publication.userId)">
+            <img src="../assets/icons/ellipsis-h-solid.svg">
+          </button>
         </div>
-        <p class="publication__title">
-          {{ publication.title }}
-        </p>
-        <div class="publication__attachement">image</div>
-        <div class="publication__footer">
-          <div class="publication__like">
-            <div>
-              like {{ publication.like }}
+        <div class="content">
+          <p class="content__title">
+            {{ publication.title }}
+          </p>
+          <a href="#image">
+            <img class="content__attachement" :src="publication.attachement">
+          </a>
+          <div class="content__footer">
+            <div class="like">
+              <div>
+                <button @click="liked(publication.id, index)"><img src="../assets/icons/thumbs-up-regular.svg"></button>
+                {{ publication.like }}
+              </div>
+              <div>
+                <button @click="disliked(publication.id, index)"><img src="../assets/icons/thumbs-down-regular.svg"></button>
+                {{ publication.dislike }}
+              </div>
             </div>
             <div>
-              dislike {{ publication.dislike }}
+              <img src="../assets/icons/comment-alt-regular.svg">
+              {{ publication.totalComments }} commentaires
             </div>
           </div>
-          <div>
-            {{ publication.totalComments }} commentaires
+        </div>
+      </div>
+    </div>
+    <div class="onTop">
+      <div class="onTop__content">
+        <div class="best">
+          <span>Meilleure publication</span>
+          <div class="best__detail">
           </div>
         </div>
-        </a>
+        <div class="horizontal_line"></div>
+        <div class="best">
+          <span>Contributeur au <img src="../assets/icons/hand-point-up-solid.svg"></span>
+          <div class="best__detail">
+          </div>
+        </div>
       </div>
     </div>
     <!-- <HelloWorld msg="Bienvenue sur Groupomania"/> -->
-  </div>
+  </main>
 </template>
 
 <script>
 //import HelloWorld from '@/components/HelloWorld.vue'
 import PublicationDataService from "../services/PublicationDataService";
+import { formatRelative } from 'date-fns'
+import { fr } from 'date-fns/locale'
 
 export default {
   name: 'Home',
@@ -45,20 +71,19 @@ export default {
   }*/
   data() {
     return {
-      publications: []
-/*
-      id: null,
-      title: null,
-      message: null,
-      attachement: null,
-      like: null,
-      dislike: null,
-      createdAt: null,
-      updatedAt: null,
-      userId: null,
-      publishedBy: null,
-      totalComments: null
-*/
+      publications: [{
+        id: null,
+        title : null,
+        publishedBy: null,
+        userId: null,
+        createdAt: null,
+        attachement: null,
+        like: null,
+        dislike: null,
+        totalComments: null
+      }],
+      //publications: [],
+      publicationUser: false,
     };
   },
   beforeMount() {
@@ -68,10 +93,47 @@ export default {
     setup() {
       PublicationDataService.findAll()
         .then(response => {
-          this.publications = response.data         
+          this.publications = response.data       
         })
         .catch(e => {
           console.log(e);
+        });
+    },
+    formatDate(date) {
+      return formatRelative(new Date(date), new Date(), { locale: fr })
+    },
+    access(userId) {
+      if (localStorage.user) {
+        let user = JSON.parse(localStorage.getItem('user'));
+        if (user.Id === userId) {
+          return true
+        }
+      }
+      return false
+    },
+    liked(id, index) {
+      const data = {
+        like: 1
+      }
+      PublicationDataService.like(id, data)
+        .then(
+          this.publications[index].like += 1
+        )
+        .catch(e => {
+          console.log(e.response.data);
+        });
+    },
+    disliked(id, index) {
+      const data = {
+        like: -1
+      }
+      PublicationDataService.like(id, data)
+        .then(response => {
+          this.publications[index].dislike -= 1
+          console.log(response)       
+        })
+        .catch(e => {
+          console.log(e.response);
         });
     }
   }
@@ -80,16 +142,28 @@ export default {
 
 <style lang="scss">
 @font-face {
-  font-family: "Roboto-Thin";
-  src: local("Roboto-Thin"),
-  url(../fonts/Roboto-Thin.ttf) format("truetype");
+  font-family: "Roboto-Regular";
+  src: local("Roboto-Regular"),
+  url(../fonts/Roboto-Regular.ttf) format("truetype");
 }
-
+.home {
+  padding-left: 120px;
+  background-color: white;
+}
+.publish {
+  & button {
+    padding-top: 50px;
+    background-color: white;
+    border: none;
+    font-size: 30px;
+    color: #909090;   
+  }
+}
 .publication {
-  font-size: 25px;
+  font-family: "Roboto-Regular";
+  font-size: 30px;
   padding-top: 50px;
   padding-bottom: 50px;
-  padding-left: 60px;
   width: 55%;
   &__wrap {
     padding: 10px;
@@ -100,26 +174,87 @@ export default {
   &__head {
     display: flex;
     justify-content: space-between;
+    & button {
+      cursor: pointer;
+      background: #909090;
+      border: none;
+      & img {
+        width: 30px;
+      }
+    }
   }
+}
+.content {
+  margin: 0px 15px;
   &__title {
-    margin: 5px 10px;
+    margin: 5px 0px;
+    color: white;
   }
   &__attachement {
-    margin: 0px 10px;
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
   }
   &__footer {
+    font-size: 25px;
     display: flex;
+    margin: 5px 0px;
     justify-content: space-between;
+    & div {
+      display: flex;
+      color: white;
+    }
+    & img {
+      width: 35px;
+      margin-right: 15px;
+    }
   }
-  &__like {
-    display: flex;
-    width: 25%;
-    justify-content: space-between;
+}
+.like {
+  display: flex;
+  width: 30%;
+  justify-content: space-between;
+  & button {
+    cursor: pointer;
+    border: none;
+    background-color: #909090;
   }
 }
 a {
   cursor: pointer;
   text-decoration: none;
   color: black;
+}
+.onTop {
+  position: fixed;
+  right: 15%;
+  top: 48%;
+  width: 296px;
+  border: 2px solid #C4C4C4;
+  &__content {
+    font-family: "Roboto-Regular";
+    font-size: 28px;
+    margin: 10px 0px;
+    color: #ff7c03;
+    text-align: center;
+    display: grid;
+  }
+}
+.best {
+  margin: 10px;
+  & img {
+    width: 25px;
+  }
+  &__detail {
+    width: 100%;
+    height: 60px;
+    margin-top: 10px;
+    background-color: #C4C4C4;
+  }
+}
+.horizontal_line {
+  margin: 10px;
+  background-color: black;
+  height: 1px;
 }
 </style>
