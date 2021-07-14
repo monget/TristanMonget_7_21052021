10470d2554320ac4bdbcca1d10160e85742e0135
monget <tristan.monget@laposte.net>
2021-07-14 21:49:00 +0200

    added of multer

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/users');
+const multer = require('../middleware/multer-config');
 const validator = require('../middleware/validator');
 const check = require('../middleware/checkExist');
 const accountLimit = require('../middleware/accountLimiter');
@@ -10,7 +11,7 @@ const auth = require('../middleware/auth');
 router.post('/signup', accountLimit, check.user, validator.signup, userCtrl.signup);
 router.post('/login', accountLimit, userCtrl.login);
 router.get('/profil/:id', auth, check.user, userCtrl.getUser);
-router.put('/profil/:id', auth, validator.signup, check.user, userCtrl.editProfil);
+router.put('/profil/:id', auth, multer, validator.signup, check.user, userCtrl.editProfil);
 router.delete('/profil/:id', auth, userCtrl.deleteProfil);
 
 module.exports = router;
