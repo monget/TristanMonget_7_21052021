d441d6ca0b1384b5c62d6dd5ae6a62facebd606e
monget <tristan.monget@laposte.net>
2021-07-15 19:18:42 +0200

    added id in models

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -1,5 +1,10 @@
 module.exports = (sequelize, Sequelize) => {
   const Comment = sequelize.define("comments", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     message: {
       type: Sequelize.TEXT
     },
--- backend/models/publication.js
+++ backend/models/publication.js
@@ -1,5 +1,10 @@
 module.exports = (sequelize, Sequelize) => {
   const Publication = sequelize.define("publications", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     message: {
       type: Sequelize.TEXT
     },
