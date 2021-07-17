2a481bd41c3e998630f658895087bfb6a3607a11
monget <tristan.monget@laposte.net>
2021-07-17 20:08:51 +0200

    change name of validator

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,10 +8,10 @@ const check = require('../middleware/checkExist');
 const accountLimit = require('../middleware/accountLimiter');
 const auth = require('../middleware/auth');
 
-router.post('/signup', accountLimit, check.user, validator.signup, userCtrl.signup);
+router.post('/signup', accountLimit, check.user, validator.user, userCtrl.signup);
 router.post('/login', accountLimit, userCtrl.login);
 router.get('/profil/:id', auth, check.user, userCtrl.getUser);
-router.put('/profil/:id', auth, multer, validator.signup, check.user, userCtrl.editProfil);
+router.put('/profil/:id', auth, multer, validator.userUpdate, check.user, userCtrl.editProfil);
 router.delete('/profil/:id', auth, userCtrl.deleteProfil);
 
 module.exports = router;
