b22f893549e00efc162d822cc507eb206a438303
monget <tristan.monget@laposte.net>
2021-07-17 20:08:10 +0200

    added validator for update user

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -1,8 +1,21 @@
 const { check, validationResult } = require('express-validator');
 const fs = require('fs');
 
-exports.signup = [
+exports.user = [
+	check('pseudo')
+	.trim()
+	.isLength({ min: 3, max: 12 }),
+		(req, res, next) => { // Contrôle du pseudo
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({ pseudo: ["Merci de renseigner un pseudo valide !"] })
+			}
+			else {
+				next();
+			}
+		},
 	check('email')
+	.trim()
 	.isEmail()
 	.normalizeEmail()
 	.matches(/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/),
@@ -17,13 +30,58 @@ exports.signup = [
 		},
 	check('password')
 	.matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-zÀ-ÖØ-öø-ÿ\d@$!%*?&\s_-]{8,}$/),
-		(req, res, next) => { // Contrôle du password
+		(req, res, next) => {
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) {
 				return res.status(400).json({ password: ["Le mot de passe doit contenir au minimun 8 caractères dont un chiffre, une lettre majuscule et une minuscule."] })
 			}
 			else {
 				next();
+			}
+		}
+];
+
+exports.userUpdate = [
+	check('pseudo')
+	.trim()
+	.isLength({ min: 3, max: 12 }),
+		(req, res, next) => { // Contrôle du pseudo
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({ pseudo: ["Merci de renseigner un pseudo valide !"] })
+			}
+			else {
+				next();
+			}
+		},
+	check('email')
+	.trim()
+	.isEmail()
+	.normalizeEmail()
+	.matches(/^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/),
+		(req, res, next) => { // Contrôle de l'email
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return res.status(400).json({ email: ["Merci de renseigner une adresse email valide !"] })
+			}
+			else {
+				next();
+			}
+		},
+	check('birthday')
+	.isISO8601(),
+		(req, res, next) => { // Contrôle de la date anniversaire
+			if (req.body.birthday == undefined) {
+				next();
+			}
+			else {
+				const errors = validationResult(req);
+				if (!errors.isEmpty()) {
+					return res.status(400).json({ birthday: ["Merci de renseigner une date valide !"] })
+				}
+				else {
+					next();
+				}
 			}
 		}
 ];
