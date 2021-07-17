99edcf97a28b4e11a02bea834fc9e7966c264468
monget <tristan.monget@laposte.net>
2021-07-17 20:10:33 +0200

    added CommentDataService

---
+++ frontend/src/services/CommentDataService.js
@@ -0,0 +1,25 @@
+import http from "../http-common";
+
+class CommentDataService {
+  create(data) {
+    return http.post("/comments", data);
+  }
+
+  update(id, data) {
+    return http.put(`/comments/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`/comments/${id}`);
+  }
+
+  findOne(id) {
+    return http.get(`/comments/${id}`);
+  }
+
+  like(id, data) {
+    return http.post(`/comments/${id}/like`, data);
+  }
+}
+
+export default new CommentDataService();
