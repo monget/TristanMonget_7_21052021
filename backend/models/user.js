462994bd37a522381b76dc0279e0cf87406db024
monget <tristan.monget@laposte.net>
2021-07-21 23:40:37 +0200

    added id and rules

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,21 +1,29 @@
 module.exports = (sequelize, Sequelize) => {
-    const User = sequelize.define("users", {
-      pseudo: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      attachement: {
-        type: Sequelize.STRING
-      },
-      birthday: {
-        type: Sequelize.DATE
-      }
-    });
-  
-    return User;
-  };
+  const User = sequelize.define("users", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    pseudo: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    attachement: {
+      type: Sequelize.STRING
+    },
+    birthday: {
+      type: Sequelize.DATE
+    },
+    isAdmin: {
+      type: Sequelize.BOOLEAN,
+    }
+  });
+
+  return User;
+};
