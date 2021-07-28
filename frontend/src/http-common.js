1fae9efe0f0604d18cb2873de78bca2315961455
monget <tristan.monget@laposte.net>
2021-07-28 22:42:18 +0200

    added interceptor for status 401 and 404

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,19 +1,36 @@
-import axios from "axios";
+import axios from "axios"
+import router from './router'
+import store from './store'
 
 function authHeader() {
   if (localStorage.user) {
-    let user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem('user'));
     
     if (user.token) {
       return { Authorization: 'Bearer ' + user.token };
     }
     else {
-      return {};
+      return {}
     }
   }
 }
 
-export default axios.create({
+const http = axios.create({
   baseURL: "http://localhost:3000/api",
   headers: authHeader(), "Content-type": "application/json"
-});
+})
+
+http.interceptors.response.use(function (response) {
+  return response
+}, function (e) {
+  if (e.response.status === 401) {
+    store.dispatch('auth/logout');
+    router.push('/connection');
+  }
+  else if (e.response.status === 404) {
+    router.push('/404')
+  }
+  return Promise.reject(e)
+})
+
+export default http;
