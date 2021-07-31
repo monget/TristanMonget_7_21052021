f0aba6b5aa81ff8a1b3c47e12da24e3a8584d09b
monget <tristan.monget@laposte.net>
2021-07-31 15:50:00 +0200

    added navigation menu for mobiles

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -14,6 +14,29 @@
       <router-link to="/registration">Inscription</router-link>
       <router-link to="/connection">Connection</router-link>
     </div>
+    <div class="navigation-mobile" role="navigation" aria-label="menu principal">
+      <button class="access-navigation" aria-label="Ouvrir le menu de navigation" @click="showNavigation()">
+        <img class="access-navigation__img" alt="logo du menu de navigation" src="../assets/icons/home-solid.svg">
+      </button>
+      <div class="navigation-menu" v-if="navigationMenu">
+        <p aria-label="pseudo de l'utilisateur" v-if="logged"> {{ this.$store.state.auth.user.pseudo}} </p>
+        <div class="navigation-menu-btn" role="navigation" aria-label="menu de navigation" v-if="logged">
+          <router-link v-if="!profil" :to="`/profil/${id()}`" @click.native="showNavigation()">Profil</router-link>
+          <router-link v-else to="/publications" @click.native="showNavigation()">Accueil</router-link>
+          <a href @click="logOut">Déconnection</a>
+        </div>
+        <div class="navigation-menu-btn" role="navigation" aria-label="menu de navigation" v-else>
+          <router-link to="/registration" @click.native="showNavigation()">Inscription</router-link>
+          <router-link to="/connection" @click.native="showNavigation()">Connection</router-link>
+        </div>
+        <ul>
+          <li><router-link to="conditions" @click.native="showNavigation()">Conditions générales</router-link></li>
+          <li><router-link to="confidentialité" @click.native="showNavigation()">Confidentialité</router-link></li>
+          <li><router-link to="contact" @click.native="showNavigation()">Contact</router-link></li>
+        </ul>
+        <span>© Groupomania 2021</span>
+      </div>
+    </div>
   </header>
 </template>
 
@@ -22,7 +45,8 @@ export default {
   name: 'Header',
   data() {
     return {
-      profil: ''
+      profil: '',
+      navigationMenu: false
     }
   },
   computed: {
@@ -52,6 +76,14 @@ export default {
     },
     id() {
       return this.$store.state.auth.user.Id
+    },
+    showNavigation() {
+      if (this.navigationMenu == false) {
+        return this.navigationMenu = true
+      }
+      else {
+        return this.navigationMenu = false
+      }
     }
   }
 }
@@ -86,6 +118,9 @@ export default {
     color: black;
   }
 }
+.navigation-mobile {
+  display: none;
+}
 @media (min-width: 1904px) {
   .navigation-btn a {
     border-radius: 80px;
@@ -104,6 +139,48 @@ export default {
   }
   .navigation-btn {
     display: none;
+  }
+  .navigation-mobile {
+    display: flex;
+    flex-direction: column;
+  }
+  .access-navigation {
+    border: none;
+    background-color: #2d3f5d;
+    &__img {
+      width: 25px;
+    }
+  }
+  .navigation-menu {
+    position: absolute;
+    top: 5%;
+    right: 0;
+    background: #b5cff9;
+    z-index: 99;
+    border: 5px solid white;
+    padding: 3%;
+    font-size: 15px;
+    border-radius: 10px;
+    & p {
+      text-align: center;
+    }
+    & a {
+      margin-top: 15px;
+      text-decoration: none;
+      color: black;
+    }
+    & ul {
+      list-style-type: none;
+      padding: 0;
+      margin: 30px 0;
+      & li {
+        margin: 5px 0;
+      }
+    }
+  }
+  .navigation-menu-btn {
+    display: flex;
+    flex-direction: column;
   }
 }
 </style>
